6180f8b47b677f35fa20aedb0d0bec1340f33c4d
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-01-20 22:58:24 +0300

    main component styles improvement

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom'
+import './Main.css'
 
 function Main() {
   console.log()
 
   return (
-    <>
-      <h1>Main</h1>
-      <Link to="/products">
-        Магазин
-      </Link>
+    <div className="picture">
+      <div className="opacity">
+        <h1>Лакомства для счастливых собак</h1>
+        <Link to="/products">
+          <button type="button" className="btn btn-primary mt-4">
+            перейти к покупкам
+          </button>
 
-    </>
+        </Link>
+      </div>
+    </div>
   )
 }
 

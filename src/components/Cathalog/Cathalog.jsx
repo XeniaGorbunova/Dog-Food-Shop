1d694783473055aaa9051fd8b48264cef2fe28a1
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-02-03 22:52:54 +0300

    add cathalog component

---
+++ src/components/Cathalog/Cathalog.jsx
@@ -0,0 +1,15 @@
+import Products from '../Products/Products'
+import Search from '../Search/Search'
+
+function Cathalog() {
+  console.log()
+
+  return (
+    <>
+      <Search />
+      <Products />
+    </>
+  )
+}
+
+export default Cathalog

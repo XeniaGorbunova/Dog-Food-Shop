631ee814e438d96cd8894972c7e295ef87148873
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-02-04 15:03:30 +0300

    add empty cart view

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 import { useDispatch, useSelector } from 'react-redux'
 import { useQuery } from '@tanstack/react-query'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../../index.css'
 import { clearCart, getAllCartProductsSelector } from '../../redux/slices/cartSlice'
 import CartItem from '../CartItem/CartItem'
@@ -20,6 +20,7 @@ function Cart() {
   } = useQuery({
     queryKey: ['cart'],
     queryFn: () => DogFoodApiConst.getProductsByIds(cart.map((product) => product.id)),
+    enabled: (userToken !== undefined) && (userToken !== ''),
   })
   console.log(cartProducts)
   const clearCartHandler = () => {
@@ -29,25 +30,25 @@ function Cart() {
   if (isLoading) return <Loader />
   if (isError) return <p>{`Error: ${error} `}</p>
   return (
-  // {!cartProducts[1] && (
-  //   <div className="d-flex align-items-center justify-content-center flex-column">
-  //   <h1>Ваша корзина пуста</h1>
-  //   <Link to="/products">
-  //     <button type="button" className="btn btn-primary mt-4">
-  //       перейти к покупкам
-  //     </button>
-  //     </Link>
-  //     </div>
-  //   )}
-
     <div className="d-flex align-items-center justify-content-center flex-column">
+      {!cart[0] && (
+      <div className="d-flex align-items-center justify-content-center flex-column">
+        <h1>Ваша корзина пуста</h1>
+        <Link to="/products">
+          <button type="button" className="btn btn-primary mt-4">
+            перейти к покупкам
+          </button>
+        </Link>
+      </div>
+      )}
+
       {cartProducts[0] && (
         <>
           <ul className="d-flex p-2 flex-wrap align-items-center justify-content-around">
             {cartProducts.map((item) => (
               <CartItem
-                key={item.id}
-                id={item.id}
+                key={item._id}
+                id={item._id}
                 name={item.name}
                 price={item.price}
                 pictures={item.pictures}
